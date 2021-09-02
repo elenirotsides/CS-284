379f01a82655d64d75ed9ed3e6513fd7aae01566
Eleni Rotsides <43190242+elenirotsides@users.noreply.github.com>
2021-09-02 14:15:58 -0400

    lecture code

---
+++ FirstProject/src/Rectangle.java
@@ -0,0 +1,36 @@
+
+public class Rectangle {
+	
+	private double width; 
+	private double height;
+	private static int numberOfRectangles = 0;
+	
+	//Constructor, no return type for constructors, its an initializer 
+	public Rectangle(double w, double h) {
+		width = w;
+		height = h;
+	}
+	
+	//Method
+	public double area() {
+		return width * height;
+	}
+	
+	public static int getNumberOfRectangles() {
+		return numberOfRectangles;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Rectangle rect1 = new Rectangle(3.5, 2.6);
+		Rectangle rect2 = new Rectangle(7.2, 8.4);
+		
+		double ar;
+		ar = rect1.area();
+		System.out.println(ar);
+		ar = rect2.area();
+		System.out.println(ar);
+
+	}
+
+}
