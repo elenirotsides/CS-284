e1fd7eb4b9e6c77efa9592b51ceb75604fe64060
Eleni Rotsides <43190242+elenirotsides@users.noreply.github.com>
2021-09-14 09:20:08 -0400

    added polymorphism code

--- Recitation2/src/week2/Polymorphism.java
+++ Recitation2/src/week2/Polymorphism.java
@@ -1,10 +1,39 @@
 package week2;
 
+class Bird{
+	public void Sing() {
+		System.out.println("tweet");
+	}
+	
+	public void Wings() {
+		System.out.println("2");
+	}
+}
+
+class Robin extends Bird{
+	public void Sing() {
+		System.out.println("eeeeeeeeeeeee");
+	}
+	public void Wings() {
+		System.out.println("8");
+	}
+}
+
 public class Polymorphism {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Bird a = new Bird();
+		a.Sing();
+		a.Wings();
+		System.out.print("\n");
+		Bird b = new Robin();
+		b.Sing();
+		b.Wings();
+		System.out.print("\n");
+		Robin c = new Robin();
+		c.Sing();
+		c.Wings();
+		
 	}
 
 }
